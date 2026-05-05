bd4947d1348f294ed91b5b0549af3e3763a5c60c
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-05-05 12:35:34 +0800

    refactor: Add UIManager configuration for button focus to enhance UI appearance

--- src/main/java/com/store_inventory/App.java
+++ src/main/java/com/store_inventory/App.java
@@ -2,10 +2,15 @@ package com.store_inventory;
 
 import com.store_inventory.pages.AppFrame;
 import com.store_inventory.services.AppServices;
+import java.awt.Color;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
 
 public class App {
   public static void main(String[] args) {
+    UIManager.put("Button.focus", new ColorUIResource(new Color(0, 0, 0, 0)));
+
     SwingUtilities.invokeLater(() -> {
       AppServices services = new AppServices();
       TestDataSeeder.seed(services);
